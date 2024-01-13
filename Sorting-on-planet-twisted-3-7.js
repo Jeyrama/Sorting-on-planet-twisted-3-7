d9cdd65c1caf527f8f0677e4208865b34398c914
Jey <126204426+Jeyrama@users.noreply.github.com>
2024-01-13 12:36:06 -0800

    Update Sorting-on-planet-twisted-3-7.js

--- Sorting-on-planet-twisted-3-7.js
+++ Sorting-on-planet-twisted-3-7.js
@@ -21,3 +21,13 @@ You should not modify the input array!
 
 
 // Solution
+
+function sortTwisted37(array) {
+  const MAPPING = {7:3,3:7};
+
+  return array.slice().sort((a,b) => twist(a) - twist(b));
+
+  function twist(number) {
+    return parseInt(number.toString().replace(/[37]/g,=>MAPPING[a]));
+  }
+}
